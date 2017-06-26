0d699ec307c5fee9e093c5e3ceef4727e54ea93a
Anagha Deshpande <anagha@metapercept.com>
2017-06-26 12:02:14 +0530

    new commit on 26-6-17

--- admin_panel/inc/header.php
+++ admin_panel/inc/header.php
@@ -13,11 +13,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
       <ul class="nav navbar-nav navbar-right">
-<<<<<<< HEAD
         <li><a href="add-post.php"><i class="fa fa-plus-square"></i> Add Post</a></li>
-=======
-        <li><a href="posts.php"><i class="fa fa-plus-square"></i> Add Post</a></li>
->>>>>>> 09b550bcb586d14052c7e49a1014e19c2d671567
         <li><a href="add-user.php"><i class="fa fa-user-plus"></i> Add User</a></li>
         <li><a href="profile.php"><i class="fa fa-user"></i> Profile</a></li>
         <li><a href="logout.php"><i class="fa fa-power-off" aria-hidden="true"></i> Logout</a></li>
